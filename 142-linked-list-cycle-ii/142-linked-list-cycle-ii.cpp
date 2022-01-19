a3322f2a263406d2fa96d44b284206f929b8f9fc
Monu Kumar <57959076+MonuKumar1@users.noreply.github.com>
2022-01-19 21:36:23 +0530

    Time: 12 ms (37.58%), Space: 7.6 MB (73.74%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
@@ -9,25 +9,57 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-     if (head == NULL || head->next == NULL)
-        return NULL;
-    
-    ListNode *slow  = head;
-    ListNode *fast  = head;
-    ListNode *entry = head;
-    
-    while (fast->next && fast->next->next) {
-        slow = slow->next;
-        fast = fast->next->next;
-        if (slow == fast) {  
-            // return slow->next;
-            while(slow != entry) {             
-                slow  = slow->next;
-                entry = entry->next;
-            }
-            return entry;
+         ListNode* fast = head;
+        ListNode* slow = head;
+        if(head == NULL)
+        {
+            return NULL;
         }
+        if(fast->next == NULL)
+        {
+            return NULL;
+        }
+        if(fast->next->next == NULL)
+        {
+            return NULL;
+        }
+        while(fast->next && fast->next->next)
+        {
+            fast = fast->next->next;
+            slow = slow->next;
+            if(fast == slow)
+            {
+                fast = head;
+                while(fast !=slow)
+                {
+                    fast = fast->next;
+                    slow = slow->next;
+                }
+                return fast;
+            }
+        }
+        return NULL;
+        
     }
-    return NULL;      
-    }
+//      if (head == NULL || head->next == NULL)
+//         return NULL;
+    
+//     ListNode *slow  = head;
+//     ListNode *fast  = head;
+//     ListNode *entry = head;
+    
+//     while (fast->next && fast->next->next) {
+//         slow = slow->next;
+//         fast = fast->next->next;
+//         if (slow == fast) {  
+//             // return slow->next;
+//             while(slow != entry) {             
+//                 slow  = slow->next;
+//                 entry = entry->next;
+//             }
+//             return entry;
+//         }
+//     }
+//     return NULL;      
+//     }
 };
