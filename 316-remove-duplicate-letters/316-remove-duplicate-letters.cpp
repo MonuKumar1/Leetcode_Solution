d2dca94a245e1b594ce63b54fe0bbaadec2c1510
Monu Kumar <57959076+MonuKumar1@users.noreply.github.com>
2022-03-19 00:13:03 +0530

    Time: 4 ms (61.69%), Space: 6.6 MB (81.09%) - LeetHub

---
+++ 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+       
+//         unordered_map<char,vector<in>t>mp;
+        
+//         int n=s.length();
+//         for(int i=0;i<n;i++)
+//         {
+//             mp[s[i]].push_back(i);
+//         }
+        
+//         for(char ch='a';ch<='z';ch++)
+//         {
+            
+//         }
+        vector<int> cnt(26,0)  , vis(26,0);
+        
+        string res = "";
+        int n = s.size();
+        
+        for(int i = 0; i<n; ++i)
+            cnt[s[i] - 'a']++;
+        
+        for(int i = 0; i<n; ++i)
+        {
+            // decrease cnt of current character
+            cnt[s[i] - 'a']--;
+            
+            // If character is not already
+            // in answer
+            if(!vis[s[i]- 'a'])
+            {
+                // Last character > s[i]
+                // and its count > 0
+                while(res.size() > 0 && res.back() > s[i] && cnt[res.back() - 'a'] > 0)
+                {
+                    // marking letter visited
+                    vis[res.back() - 'a'] = 0;
+                    res.pop_back();
+                }
+                
+                // Add s[i] in res and
+                // mark it visited
+                res += s[i];
+                vis[s[i] - 'a'] = 1;
+            }
+        }
+        // return resultant string
+        return res;
+        
+    }
+};
