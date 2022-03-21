7477ba60cf58a8c14bbb51d6dffd813af4d4b323
Monu Kumar <57959076+MonuKumar1@users.noreply.github.com>
2022-03-21 10:47:32 +0530

    Time: 3 ms (85.70%), Space: 6.7 MB (64.08%) - LeetHub

--- 763-partition-labels/763-partition-labels.cpp
+++ 763-partition-labels/763-partition-labels.cpp
@@ -1,50 +1,50 @@
-// class Solution {
-// public:
-//     vector<int> partitionLabels(string s) {
-//         int n=s.size();
-//       vector<int>end_ind(26,0),res;
-//         for(int i=0;i<n;i++)
-//         {
-//             end_ind[s[i]-'a']=i;
-//         }
-//         int start=0,end=0;
-//         for(int i=0;i<n;i++)
-//         {
-//             end=max(end_ind[s[i]-'a'],end);
-//             if(i==end)
-//             {
-//                 res.push_back(end-start+1);
-//                 start=i+1;
-//             }
-//         }
-//         return res;
-//     }
-// };
 class Solution {
 public:
     vector<int> partitionLabels(string s) {
-        vector<int>v;
-        int i,j,k,l,m,n;
-        n=s.length();
-        unordered_map<char,int>um;
-        for(i=0;i<n;i++)
+        int n=s.size();
+      vector<int>end_ind(26,0),res;
+        for(int i=0;i<n;i++)
         {
-            um[s[i]]++;
+            end_ind[s[i]-'a']=i;
         }
-        k=0;
-        for(i=0;i<n;i++)
+        int start=0,end=0;
+        for(int i=0;i<n;i++)
         {
-            um[s[i]]--;
-            if(um[s[i]]==0)
+            end=max(end_ind[s[i]-'a'],end);
+            if(i==end)
             {
-                for(j=0;j<=i;j++)
-                {
-                    if(um[(s[j])]>0)break;
-                }
-                if(j==i+1){v.push_back(i+1-k);k=i+1;}
+                res.push_back(end-start+1);
+                start=i+1;
             }
-            
         }
-        return v;
+        return res;
     }
 };
+// class Solution {
+// public:
+//     vector<int> partitionLabels(string s) {
+//         vector<int>v;
+//         int i,j,k,l,m,n;
+//         n=s.length();
+//         unordered_map<char,int>um;
+//         for(i=0;i<n;i++)
+//         {
+//             um[s[i]]++;
+//         }
+//         k=0;
+//         for(i=0;i<n;i++)
+//         {
+//             um[s[i]]--;
+//             if(um[s[i]]==0)
+//             {
+//                 for(j=0;j<=i;j++)
+//                 {
+//                     if(um[(s[j])]>0)break;
+//                 }
+//                 if(j==i+1){v.push_back(i+1-k);k=i+1;}
+//             }
+            
+//         }
+//         return v;
+//     }
+// };
