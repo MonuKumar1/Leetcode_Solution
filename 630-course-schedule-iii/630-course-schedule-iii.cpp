0fb38741431cd43beb2de15bae7b2cb07f202715
Monu Kumar <57959076+MonuKumar1@users.noreply.github.com>
2022-06-24 10:12:55 +0530

    Time: 598 ms (15.91%), Space: 63.4 MB (34.25%) - LeetHub

--- 630-course-schedule-iii/630-course-schedule-iii.cpp
+++ 630-course-schedule-iii/630-course-schedule-iii.cpp
@@ -1,16 +1,24 @@
+bool cmp(const vector<int>& v1, const vector<int>& v2)
+    {
+     return v1[1] < v2[1];
+    }
 class Solution {
+
 public:
+
     int scheduleCourse(vector<vector<int>>& courses) {
+        
         if(courses.size() <= 0) return 0;
-        sort(courses.begin(), courses.end(), [](const vector<int>& a, vector<int>& b) {
-            return a[1] < b[1];
-        });
+        
+        sort(courses.begin(),courses.end(),cmp);
+        
         priority_queue<int> q;
+        
         int sum = 0;
-        for(auto i : courses) {
-            sum += i[0];
-            q.push(i[0]);
-            if(sum > i[1]) {
+        for(auto it : courses) {
+            sum += it[0];
+            q.push(it[0]);
+            if(sum > it[1]) {
                 sum -= q.top();
                 q.pop();
             }
