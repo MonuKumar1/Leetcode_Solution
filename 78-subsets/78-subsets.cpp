e748dbefd3ebebc12095793d56ed1b696ee34c49
Monu Kumar <57959076+MonuKumar1@users.noreply.github.com>
2022-07-29 18:30:59 +0530

    Time: 5 ms (43.24%), Space: 7.2 MB (56.19%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,51 +1,20 @@
 class Solution {
 public:
-    vector<vector<int>> subsets(vector<int>& nums) {
-        
-        // vector<vector<int>> res;
-        // // vector<int>curr={};
-        // // helper(0,nums,curr,res);
-        // return res;
-        
-     vector<vector<int>> subs = {{}};
-
-        for(int i=0;i<nums.size();i++){
-            
-            int n=subs.size();
-            
-            for(int j=0;j<n;j++)
-            {
-                    vector<int>temp=subs[j];
-                    temp.push_back(nums[i]);
-                    subs.push_back(temp);
-            }
+    vector<vector<int>>subset;
+    void helper(int index, vector<int>&current,vector<int>&nums)
+    {
+        subset.push_back(current); 
+        for(int i=index;i<nums.size();i++)
+        {
+            current.push_back(nums[i]); 
+            helper(i+1,current,nums); 
+            current.pop_back(); 
         }
-        return subs;
-        
-    }  
-    
-    
+        return;
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int>current;
+        helper(0,current,nums);
+        return subset; 
+    }
 };
-// void fff(vector<int>&v,vector<int>cur,vector<vector<int>>&ans,int i,int n)
-// {
-//     if(i>=n)
-//     {
-//         ans.push_back(cur);return;
-//     }
-//     cur.push_back(v[i]);
-//     fff(v,cur,ans,i+1,n);
-//     cur.pop_back();
-//     fff(v,cur,ans,i+1,n);
-// }
-
-// class Solution {
-// public:
-//     vector<vector<int>> subsets(vector<int>&v) {
-//         int i,j,k,l,m,n;
-//         n=v.size();
-//         vector<vector<int>>ans;
-//         vector<int>cur;
-//         fff(v,cur,ans,0,n);
-//         return ans;
-//     }
-// };
