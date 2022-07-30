f55fb655506a2e3bf3981459b3faa10a932ab01c
Monu Kumar <57959076+MonuKumar1@users.noreply.github.com>
2022-07-30 17:25:30 +0530

    Time: 132 ms (91.44%), Space: 39.9 MB (84.76%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -1,33 +1,48 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<pair<int,int>> adj[n+1];
+        
+        vector<pair<int,int>>adj[n+1];
+        
         for(int i=0;i<times.size();i++)
-                adj[times[i][0]].push_back({times[i][1],times[i][2]});
-        vector<int> dist(n+1,INT_MAX);
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+        {
+            adj[times[i][0]].push_back({times[i][1],times[i][2]});
+        }
+        
+        vector<int>dis(n+1,INT_MAX);
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+        
+        dis[k]=0;
         pq.push({0,k});
-        dist[k]=0;
-        while(!pq.empty())
-        {
-            pair<int,int> t=pq.top();
+        
+        while(pq.size()){
+            
+            int node= pq.top().second;
+            int cur_dis= pq.top().first;
             pq.pop();
-            for(pair<int,int> it:adj[t.second])
+            
+            
+            for(auto it:adj[node])
             {
-                if(dist[it.first]>t.first+it.second)
+                int cur_node =it.first;
+                int wt=it.second;
+                
+                if(dis[cur_node]>wt+cur_dis)
                 {
-                    dist[it.first]=t.first+it.second;
-                    pq.push({dist[it.first],it.first});
+                    dis[cur_node]=wt+cur_dis;
+                    
+                    pq.push({dis[cur_node],cur_node});
                 }
+                
             }
+            
+            
         }
-        int res=0;
-        for(int i=1;i<=n;i++)
-        {
-            if(dist[i]==INT_MAX)
-                return -1;
-            res=max(res,dist[i]);
-        }
-		return res;
-	}
+        
+        int ans =*max_element(dis.begin()+1,dis.end());
+        
+        return ans==INT_MAX?-1:ans;
+        
+        
+    }
 };
