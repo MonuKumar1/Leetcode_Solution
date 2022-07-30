1a907bfbba15f173cf890059302b95ecace0037f
Monu Kumar <57959076+MonuKumar1@users.noreply.github.com>
2022-07-30 18:43:11 +0530

    Time: 40 ms (71.43%), Space: 13.4 MB (62.66%) - LeetHub

---
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.cpp
@@ -0,0 +1,67 @@
+class Solution {
+public:
+
+    
+    
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        
+        vector<pair<int,int>> adj[n];
+        
+        int len =  flights.size();
+        
+        for(int i=0;i<len;i++)
+        {
+            adj[flights[i][0]].push_back({flights[i][1],flights[i][2]});
+        } 
+       
+        priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>> > pq;
+        
+        vector<int>dis(n,INT_MAX),vk(n,INT_MIN);
+        dis[src]=0;
+        pq.push({0,src,k+1});
+        vk[src]=k+1;
+        
+        
+        while(!pq.empty())
+        {
+            int cur_dis=pq.top()[0];
+            int cur_node=pq.top()[1];
+            int cur_k=pq.top()[2];
+            
+            pq.pop();
+            
+            
+            
+            if(cur_k<=0)continue;
+            else if(cur_k<vk[cur_node])continue;
+            
+            vk[cur_node] = cur_k;
+            
+            for(auto it:adj[cur_node])
+            {
+                if(dis[it.first]>cur_dis+it.second)
+                {
+                    dis[it.first]=cur_dis+it.second;
+                    pq.push({dis[it.first],it.first,cur_k-1});
+                    // vk[cur_node]=cur_k-1;
+                    vk[it.first]=cur_k-1;
+                }
+                else if(vk[it.first]<cur_k-1)
+                {
+                     pq.push({cur_dis+it.second,it.first,cur_k-1});
+                    
+                }
+            }
+        }
+        
+        return dis[dst]==INT_MAX?-1:dis[dst];
+        
+        
+        
+        
+        // shortesPath(src,n,dst,k,adj);
+        
+        
+        
+    }
+};
