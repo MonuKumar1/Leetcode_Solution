0efcfbafd529523dead71a3d3a67516db1209240
Monu Kumar <57959076+MonuKumar1@users.noreply.github.com>
2022-08-03 17:08:26 +0530

    Time: 14 ms (47.59%), Space: 8.6 MB (95.83%) - LeetHub

---
+++ 684-redundant-connection/684-redundant-connection.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    vector<int>parent,rank;
+    int x,y;
+    int findP(int u)
+    {
+        if(parent[u]==u)
+        {return u;}
+        
+        return parent[u]=findP(parent[u]);
+        
+    }
+    
+    void unions(int u,int v)
+    {
+        int Pu=findP(u),Pv=findP(v);
+        
+        parent[Pu]=Pv;
+    }
+    
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        
+        int n=edges.size();
+        parent.resize(n+1);
+        for(int i=1;i<=n;i++)
+        {
+            parent[i]=i;
+        }
+        vector<int>v(2);
+        
+        for(int i=0;i<n;i++)
+        {
+            int a=edges[i][0];
+            int b=edges[i][1];
+            
+            if(findP(a)==findP(b))
+            {
+                v[0]=a;
+                v[1]=b;  
+                return v;
+            }
+            unions(a,b);          
+            
+        }
+        return v;
+        
+    }
+};
