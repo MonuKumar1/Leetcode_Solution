68e3afd3212a91291df7d713c4dcdee074a2c6bd
Monu Kumar <57959076+MonuKumar1@users.noreply.github.com>
2022-08-03 09:39:21 +0530

    Time: 149 ms (62.19%), Space: 37.6 MB (87.88%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.cpp
+++ 729-my-calendar-i/729-my-calendar-i.cpp
@@ -8,7 +8,7 @@ public:
     bool book(int start, int end) {
         
         
-        for (pair<int, int> p : books)
+        for (auto p : books)
             if (max(p.first, start) < min(end, p.second)) return false;
         books.push_back({start, end});
         return true;
