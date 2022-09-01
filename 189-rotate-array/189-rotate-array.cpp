731565dab137d7ad5b10e05c615fcec9d35bf681
Monu Kumar <57959076+MonuKumar1@users.noreply.github.com>
2022-09-01 15:11:10 +0530

    Time: 54 ms (37.06%), Space: 24.8 MB (99.32%) - LeetHub

--- 189-rotate-array/189-rotate-array.cpp
+++ 189-rotate-array/189-rotate-array.cpp
@@ -2,25 +2,17 @@ class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
         int n=nums.size();
-        if ((n == 0) || (k <= 0))return;
-         
-            deque<int>q;
-            for(int i=0;i<n;i++)
-            {
-                q.push_back(nums[i]);
-            }
-             for(int i=0;i<k;i++)
-             {
-                 int temp=q.back();
-                 q.pop_back();
-                 q.push_front(temp);
-             }
-                 for(int i=0;i<n;i++)
-                 {
-                     nums[i]=q.front();
-                     q.pop_front();
-                     
-                 }
-          
+        k = k%n;
+    
+            
+            reverse(nums.begin(), nums.begin() + n - k);
+            
+            
+            reverse(nums.begin() + n - k, nums.begin() + n);
+            
+           
+            reverse(nums.begin(), nums.begin() + n);
+               
+               
     }
 };
