e1ccaf3f86ca36f5d297a85f755f4a5ed1d03512
Monu Kumar <57959076+MonuKumar1@users.noreply.github.com>
2022-09-02 23:47:16 +0530

    Time: 1442 ms (6.00%), Space: 231.1 MB (20.10%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -2,15 +2,17 @@ class Solution {
 public:
     string longest_common_substring(string str1, string str2) 
     {   
-        int s1 = str1.size(); int s2 = str2.size();
+        int s1 = str1.size(); 
+        int s2 = str2.size();
         int dp[s1+1][s2+1];
-        string res ; int max = 0 ;
+        string res ; 
+        int max = 0 ;
 
         for(int i=0; i<=s1; i++)
         {
             for(int j=0; j<=s2; j++)
-            {   if(i==0 || j==0){ dp[i][j] = 0;}
-             
+            {   
+                if(i==0 || j==0){ dp[i][j] = 0;}
                 else if(str1[i-1] == str2[j-1])
                 {
                     dp[i][j] = 1 + dp[i-1][j-1];
@@ -20,12 +22,9 @@ public:
                         string temp = str1.substr(i-dp[i][j], dp[i][j]) ;
                         string revtemp=temp;
                         reverse(revtemp.begin(),revtemp.end());
-                        // string revtemp = string(temp.rbegin(),temp.rend());
                         if(revtemp==temp)
-                        {
-                            max = dp[i][j] ;
-                            res = temp ;
-                        }
+                          { max = dp[i][j] ;
+                            res = temp ; }
                     }
                 }
                 else
