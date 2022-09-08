0eb1b3014d73736054562ff8c7e00de1832c0a94
Monu Kumar <57959076+MonuKumar1@users.noreply.github.com>
2022-09-08 15:28:10 +0530

    Time: 30 ms (67.39%), Space: 18.2 MB (22.57%) - LeetHub

--- 135-candy/135-candy.cpp
+++ 135-candy/135-candy.cpp
@@ -1,39 +1,8 @@
 class Solution {
 public:
     int candy(vector<int>& ratings) {
-        
-//         int n=vec.size();
-//         vector<int>v(n,1);
-//         int ans=0;
-        
-//         for(int i=1;i<n;i++)
-//         {
-//             if(vec[i]<vec[i-1])
-//             {
-//                 v[i-1]+=1;
-                
-//             }
-//             else if(vec[i]>vec[i-1])
-//                 v[i]+=1;
-//         }
-        
-//         for(int i=n-1;i>0;i--)
-//         {
-//             if(vec[i]<vec[i-1] and v[i]>v[i-1])
-//             {
-//                 v[i-1]+=1;
-                
-//             }
-//            else if(vec[i]>vec[i-1] and v[i]<=v[i-1] )
-//                 v[i]+=1;
-//         }
-        
-//         for(auto x:v){
-//             // cout<<x<<" ";
-//             ans+=x;
-//         }
-//         return ans;
-            int n = ratings.size();
+ 
+        int n = ratings.size();
         if(n == 1)return 1;
         vector<int>l(n, 1), r(n, 1);
         for(int i = 0; i < n-1; i++)
