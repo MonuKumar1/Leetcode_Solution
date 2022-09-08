82f4695c4061368410b1311f76e84a90eb281c17
Monu Kumar <57959076+MonuKumar1@users.noreply.github.com>
2022-09-08 11:54:24 +0530

    Time: 147 ms (41.92%), Space: 29.5 MB (41.16%) - LeetHub

--- 887-super-egg-drop/887-super-egg-drop.cpp
+++ 887-super-egg-drop/887-super-egg-drop.cpp
@@ -1,50 +1,52 @@
-class Solution {
-public:
-    int fun(int e,int f,vector<vector<int>>&dp){
-        
-        if(f==0 || f==1)return f;
-        
-        if(e==1)return f;
-        if(dp[e][f]!=-1)return dp[e][f];
-        
-        int mini=1e9;
-        // for(int k=1;k<=f;k++)
-        int l=1,h=f,temp=0;
-        while(l<=h)
+class Solution
+{
+    public:
+        int fun(int e, int f, vector<vector < int>> &dp)
         {
-            int k=(l+h)/2;
-            int flag=0;
-            int low,high;
-            if(dp[e-1][k-1]!=-1)
-                low=dp[e-1][k-1];
-            else {
-                dp[e-1][k-1]=fun(e-1,k-1,dp);
-                low=dp[e-1][k-1];
+
+            if (f == 0 || f == 1) return f;
+
+            if (e == 1) return f;
+            if (dp[e][f] != -1) return dp[e][f];
+
+            int mini = 1e9;
+            int l = 1, h = f, temp = 0;
+            while (l <= h)
+            {
+                int k = (l + h) / 2;
+                int flag = 0;
+                int low, high;
+                if (dp[e - 1][k - 1] != -1)
+                    low = dp[e - 1][k - 1];
+                else
+                {
+                    dp[e - 1][k - 1] = fun(e - 1, k - 1, dp);
+                    low = dp[e - 1][k - 1];
+                }
+                if (dp[e][f - k] != -1)
+                    high = dp[e][f - k];
+                else
+                {
+                    dp[e][f - k] = fun(e, f - k, dp);
+                    high = dp[e][f - k];
+                }
+
+                temp = 1 + max(low, high);//worst case
+
+                if (low < high) l = k + 1;
+
+                else h = k - 1;
+
+                mini = min(mini, temp);// worst case mai kitne minimum attempts lagenge
             }
-            if(dp[e][f-k]!=-1)
-                high=dp[e][f-k];
-            else {
-                dp[e][f-k]=fun(e,f-k,dp);
-                high=dp[e][f-k];
-            }
-            
-        
-            temp= 1+ max(low,high);
-            
-            if(low<high)l=k+1;
-            
-            else h=k-1;
-            
-            mini=min(mini,temp);
+
+            return dp[e][f] = mini;
         }
-        
-        return dp[e][f]=mini;
-    }
-    
-    int superEggDrop(int k, int n) {
-        
-        vector<vector<int>>dp(k+1,vector<int>(n+1,-1));
-        return fun(k,n,dp);
-        
+
+    int superEggDrop(int k, int n)
+    {
+
+        vector<vector < int>> dp(k + 1, vector<int> (n + 1, -1));
+        return fun(k, n, dp);
     }
 };
