e9353b652c19a48c3f6bf8460a5528e1b72087c3
Monu Kumar <57959076+MonuKumar1@users.noreply.github.com>
2022-09-10 15:01:34 +0530

    Time: 0 ms (100.00%), Space: 6 MB (68.30%) - LeetHub

--- 650-2-keys-keyboard/650-2-keys-keyboard.cpp
+++ 650-2-keys-keyboard/650-2-keys-keyboard.cpp
@@ -1,5 +1,8 @@
 class Solution {
 public:
+    
+    // my logic! jabtak even hai half krte raho odd aane par uska largest prime factor se divide
+    //not my code below!
     int minSteps(int n) {
         
      int primes[11] = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31 };
