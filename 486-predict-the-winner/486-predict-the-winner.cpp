06efe2e19b6a0c23a9ff2128e1bbacbda8f8c8fa
Monu Kumar <57959076+MonuKumar1@users.noreply.github.com>
2022-09-11 12:25:28 +0530

    Time: 401 ms (6.13%), Space: 7.5 MB (65.95%) - LeetHub

---
+++ 486-predict-the-winner/486-predict-the-winner.cpp
@@ -0,0 +1,57 @@
+// class Solution {
+// public:
+//     int fun(int i,int j,int turn,int pl1,vector<int>&v)
+//     {
+//         if(i>=j)return 0;
+//         int pturn=turn?1:0;
+//         if(turn){
+          
+//             int l= fun(i+1,j,0,pl1+v[i],v);
+//             int r=fun(i,j-1,0,pl1+v[j],v);
+            
+//             return max(l,r);
+//         }
+//         else  { 
+        
+//              int l= fun(i+1,j,1,pl1-v[i],v);
+//             int r=fun(i,j-1,1,pl1-v[j],v);
+            
+//             return min(l,r);
+//         }
+//     }
+//     bool PredictTheWinner(vector<int>& v) {
+//         int pl1=0,pl2=0;
+        
+//         int ans=fun(0,v.size()-1,1,0,v);
+//         cout<<pl1<<"   "<<pl2<<endl;
+//         return ans>=0;
+        
+//     }
+// };
+class Solution {
+    public:    
+     bool PredictTheWinner(vector<int>& v) {
+        return fun(0, v.size() - 1, 0, v, true) >= 0;
+    }
+
+   int fun(int start, int end, int p1, vector<int>&nums, bool p1Turn) {
+        if( start== end )
+            {
+            if( p1Turn ) return p1+ nums[start];
+            return p1 - nums[start];
+            }
+        
+        int turn = p1Turn ? 1: 0;
+        
+        if (p1Turn) {
+            int bottom = fun(start + 1, end, p1 + nums[start], nums, false);
+            int top = fun(start, end - 1, p1 + nums[end], nums, false);
+
+            return max(top, bottom);
+        } else {
+            int bottom = fun(start + 1, end, p1 - nums[start], nums, true);
+            int top = fun(start, end - 1, p1 - nums[end], nums, true);
+            return min(bottom, top);
+        }        
+    } 
+};
