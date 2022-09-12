3baac1c9438d3e53b43396771ade056869a53098
Monu Kumar <57959076+MonuKumar1@users.noreply.github.com>
2022-09-12 11:56:37 +0530

    Time: 0 ms (100.00%), Space: 9.7 MB (38.11%) - LeetHub

--- 226-invert-binary-tree/226-invert-binary-tree.cpp
+++ 226-invert-binary-tree/226-invert-binary-tree.cpp
@@ -1,31 +1,29 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ *Definition for a binary tree node.
+ *struct TreeNode {
+ *    int val;
+ *    TreeNode * left;
+ *    TreeNode * right;
+ *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *};
  */
-class Solution {
-public:
-    void swap1(TreeNode* cur){
-        if(!cur)
-            return;
-        swap(cur->left,cur->right);
-        swap1(cur->left);swap1(cur->right);
-        // TreeNode* t;
-        // t=cur->left;
-        // cur->left=cur->right;
-        // cur->right=t;
-    }
-    
-    TreeNode* invertTree(TreeNode* root) {
-       swap1(root);
+class Solution
+{
+    public:
+        void swap1(TreeNode *cur)
+        {
+            if (!cur)
+                return;
+            swap(cur->left, cur->right);
+            swap1(cur->left);
+            swap1(cur->right);
+        }
+
+    TreeNode* invertTree(TreeNode *root)
+    {
+        swap1(root);
         return root;
-        
-        
     }
 };
