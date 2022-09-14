d20f0c756bd3f13b0c5ea5029f38e5b8ef788546
Monu Kumar <57959076+MonuKumar1@users.noreply.github.com>
2022-09-14 16:59:38 +0530

    Time: 899 ms (35.48%), Space: 14.1 MB (68.13%) - LeetHub

--- 127-word-ladder/127-word-ladder.cpp
+++ 127-word-ladder/127-word-ladder.cpp
@@ -1,42 +1,46 @@
-class Solution {
-public:
-    int ladderLength(string beginWord, string endWord, vector<string>& wordList)
-    {
-        if(find(wordList.begin(),wordList.end(),endWord)==wordList.end())
-            return 0;
-        set<string> s;
-        for(auto i:wordList)
-            s.insert(i);
-        queue<string> q;
-        q.push(beginWord);
-        int d=0;
-        while(!q.empty())
+class Solution
+{
+    public:
+        int ladderLength(string beginWord, string endWord, vector<string> &wordList)
         {
-            d++;
-            int n=q.size();
-            while(n--)
+            if (find(wordList.begin(), wordList.end(), endWord) == wordList.end())
+                return 0;
+            set<string> s(wordList.begin(),wordList.end());
+           
+            queue<string> q;
+            
+            q.push(beginWord);
+            
+            int d = 0;
+            
+            while (!q.empty())
             {
-                string curr=q.front();
-                q.pop();
-                for(int i=0;i<curr.length();i++)
+                d++;
+                int n = q.size();
+                while (n--)
                 {
-                    string tmp=curr;
-                    for(char c='a';c<='z';c++)
+                    string curr = q.front();
+                    q.pop();
+                    for (int i = 0; i < curr.length(); i++)
                     {
-                        tmp[i]=c;
-                        if(tmp==curr)
-                            continue;
-                        if(tmp==endWord)
-                            return d+1;
-                        if(s.find(tmp)!=s.end())
+                        string tmp = curr;
+                        
+                        for (char c = 'a'; c <= 'z'; c++)
                         {
-                            q.push(tmp);
-                            s.erase(tmp);
+                            tmp[i] = c;
+                            if (tmp == curr)
+                                continue;
+                            if (tmp == endWord)
+                                return d + 1;
+                            if (s.find(tmp) != s.end())
+                            {
+                                q.push(tmp);
+                                s.erase(tmp);
+                            }
                         }
                     }
                 }
             }
+            return 0;
         }
-        return 0;
-    }
 };
