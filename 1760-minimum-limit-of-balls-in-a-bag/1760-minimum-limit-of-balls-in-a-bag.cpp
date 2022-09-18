f68eb4d3d67900f52d3315f64261c3e58c3199a1
Monu Kumar <57959076+MonuKumar1@users.noreply.github.com>
2022-09-18 11:41:46 +0530

    Time: 234 ms (86.32%), Space: 117.9 MB (5.81%) - LeetHub

--- 1760-minimum-limit-of-balls-in-a-bag/1760-minimum-limit-of-balls-in-a-bag.cpp
+++ 1760-minimum-limit-of-balls-in-a-bag/1760-minimum-limit-of-balls-in-a-bag.cpp
@@ -1,40 +1,42 @@
-class Solution {
-public:
-    
-    int helper(int m,vector<int> v)
-    {   int cur=0;
-        for(auto x:v){
-           {
-            int p=x/m;
-            if(p*m==x)cur+=(p-1);
-                else cur+=(p);
-            }
-        }
-     return cur;
-        
-    }
-    
-    
-    int minimumSize(vector<int>& nums, int k) {
-        
-        int l=1,h=*max_element(nums.begin(),nums.end());
-        int ans=h;
-        while(l<=h)
+class Solution
+{
+    public:
+
+        int helper(int m, vector<int> v)
         {
-            int m=l+(h-l)/2;
-            
-            int n=helper(m,nums);
-            
-            if(n>k)
+            int cur = 0;
+            for (auto x: v)
             {
-                l=m+1;
+                {
+                    int p = x / m;
+                    if (p *m == x) cur += (p - 1);
+                    else cur += (p);
+                }
             }
-            if(n<=k){
-            ans=m;
-                h=m-1;
+            return cur;
+        }
+
+    int minimumSize(vector<int> &nums, int k)
+    {
+
+        int l = 1, h = *max_element(nums.begin(), nums.end());
+        int ans = h;
+        while (l <= h)
+        {
+            int m = l + (h - l) / 2;
+
+            int n = helper(m, nums);
+
+            if (n > k)
+            {
+                l = m + 1;
+            }
+            if (n <= k)
+            {
+                ans = m;
+                h = m - 1;
             }
         }
         return ans;
-      
     }
 };
