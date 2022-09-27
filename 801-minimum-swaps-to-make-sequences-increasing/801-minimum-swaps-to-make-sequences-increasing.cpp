1489082fb9d9795ab978eee2ad731096090e4090
Monu Kumar <57959076+MonuKumar1@users.noreply.github.com>
2022-09-27 14:35:49 +0530

    Time: 384 ms (54.24%), Space: 150.6 MB (5.34%) - LeetHub

--- 801-minimum-swaps-to-make-sequences-increasing/801-minimum-swaps-to-make-sequences-increasing.cpp
+++ 801-minimum-swaps-to-make-sequences-increasing/801-minimum-swaps-to-make-sequences-increasing.cpp
@@ -19,6 +19,37 @@ public:
         
         
     }
+    int solveTab(vector<int>& nums1, vector<int>& nums2){
+        
+        int n=nums1.size();
+        vector<vector<int>>dp(nums1.size()+1,vector<int>(2,0));
+        
+        for(int ind=n-1;ind>=1;ind--)
+        {
+            for(int swapped=1;swapped>=0;swapped--)
+            {
+                int prev1=nums1[ind-1];
+                int prev2=nums2[ind-1];
+                int ans=1e9;
+                if(swapped)
+                    swap(prev1,prev2);
+                if(nums1[ind]>prev1 and nums2[ind]>prev2)
+                   ans = dp[ind+1][0];
+
+                if(nums1[ind]>prev2 and nums2[ind]>prev1)
+                    ans=min(ans,1+dp[ind+1][1]);
+                
+                dp[ind][swapped]=ans;
+
+                
+            
+          }
+        
+     }
+                            
+    return dp[1][0];
+}
+                            
     
     int minSwap(vector<int>& nums1, vector<int>& nums2) {
         nums1.insert(nums1.begin(),-1);
@@ -27,7 +58,7 @@ public:
         
         vector<vector<int>>dp(nums1.size()+1,vector<int>(2,-1));
         
-        return solve(nums1,nums2,1,swapped,dp);
+        return solveTab(nums1,nums2);
         // return ans;
     }
 };
