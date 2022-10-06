bc72c60b793e02294e24a918a140842014e9a7de
Monu Kumar <57959076+MonuKumar1@users.noreply.github.com>
2022-10-06 13:25:20 +0530

    Time: 0 ms (100.00%), Space: 6 MB (22.50%) - LeetHub

---
+++ 1802-maximum-value-at-a-given-index-in-a-bounded-array/1802-maximum-value-at-a-given-index-in-a-bounded-array.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool solve(   long long int x,   long long int n,int i,int maxsum){
+        long long int left=i,right=n-i-1,a=1,b=0;
+       long long int low=max(x-i,a),sum=0;
+        sum+=(1ll*x*(x+1))/2-(1ll*low*(low-1))/2;  
+        low=max(x-right,a);
+        sum+=(1ll*x*(x-1))/2-(1ll*low*(low-1))/2;
+        sum+=max(left-(x-1),b);
+        sum+=max(right-(x-1),b);
+        if(sum<=maxsum)return true;
+        return false;
+        
+    }
+    int maxValue(int n, int index, int maxSum) {
+      long long   int low=1,high=maxSum,ans=0;
+        while(low<=high){
+              long long int mid=low+(high-low)/2;
+            if(solve(mid,n,index,maxSum)){
+                ans=mid;
+                low=mid+1;
+            }
+            else high=mid-1;
+        }
+        return ans;
+    }
+    
+};
