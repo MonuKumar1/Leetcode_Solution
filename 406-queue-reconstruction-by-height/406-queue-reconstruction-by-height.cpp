e72eb9320a07434b6b3844e6e21179b4f4bc8ad1
Monu Kumar <57959076+MonuKumar1@users.noreply.github.com>
2022-10-07 18:58:45 +0530

    Time: 112 ms (75.92%), Space: 11.6 MB (95.57%) - LeetHub

--- 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.cpp
+++ 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
-    static bool cmp(vector<int>&v1,vector<int>&v2)
-    {
-        if(v1[0]==v2[0])
-            return v1[1]<v2[1];
-       return v1[0]>v2[0];
-    }
-    vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
+ 
+    vector<vector<int>> reconstructQueue(vector<vector<int>>&v) {
      
-        sort(people.begin(),people.end(),cmp);
-        
-        
-        vector<vector<int>>v;
-        
-        
-        for(int i=0;i<people.size();i++)
-        {
-            int ind=people[i][1];
-            v.insert(v.begin() + ind, people[i]); 
-        }
-        return v;
+        int n=v.size();
+       vector<vector<int>> ans(n,vector<int>(2,-1));
+       sort(v.begin(),v.end());
+       for(int i=0;i<n;i++){
+           int cnt=0;
+           for(int j=0;j<n;j++){
+              
+               if(ans[j][0]==-1 || ans[j][0]==v[i][0])cnt++;
+                if(cnt==v[i][1]+1){
+                   ans[j][0]=v[i][0];
+                   ans[j][1]=v[i][1];
+                   break;
+               }
+           }
+       }
+        return ans;
         
     }
 };
