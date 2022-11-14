e3066b502ad72d7f6348e9c91bfa16d117020bc3
Monu Kumar <57959076+MonuKumar1@users.noreply.github.com>
2022-11-15 00:46:18 +0530

    Time: 962 ms (5.01%), Space: 14.3 MB (73.69%) - LeetHub

---
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+    vector<int>parent,rank;
+    
+    int findP(int u)
+    {
+        if(parent[u]==u)return u;
+        return parent[u]=findP(parent[u]);
+    }
+    void unions(int u,int v)
+    {
+        int Pu=findP(u),Pv=findP(v);
+        
+        if(Pu!=Pv)
+        {
+            if(rank[Pu]>rank[Pv])
+            {
+                parent[Pv]=Pu;
+             }
+           else if(rank[Pu]<rank[Pv])
+            {
+                parent[Pu]=Pv;
+             }
+            else
+            {
+                parent[Pv]=Pu;
+                rank[Pu]++;
+            }
+        }
+        
+    }
+    
+    int removeStones(vector<vector<int>>& stones) {
+     
+        int n=stones.size();
+        
+        parent.resize(n);
+        rank.resize(n,0);
+        
+        for(int i=0;i<n;i++)
+        {
+            parent[i]=i;
+        }
+        
+        
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(stones[i][0]==stones[j][0] || stones[i][1]==stones[j][1])
+                    unions(i,j);
+                
+            }
+        }
+        int cnt=0;
+        for(int i=0;i<n;i++)
+        {
+            if(parent[i]==i)cnt++;
+        }
+        return n-cnt;
+      
+    }
+};
