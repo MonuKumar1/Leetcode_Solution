b831491d8570f1ae82edb4b6958c6b413ee0794b
Monu Kumar <57959076+MonuKumar1@users.noreply.github.com>
2022-11-14 09:48:29 +0530

    Time: 180 ms (43.37%), Space: 48.3 MB (78.70%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -13,19 +13,6 @@ public:
         
        return true; 
         
-        // int dp[n];
-        // memset(dp,0,sizeof(dp));
-        // int x=0;
-        // for(int i=0;i<n-1;i++)
-        // {
-        //     x=max(x,nums[i]);
-        //     if(x>0){
-        //         dp[i]=1;
-        //         }
-        //     x--;
-        // }
-        // for(int i=0;i<n-1;i++)
-        //     if(dp[i]==0)return 0;
-        // return 1;
+      
     }
 };
