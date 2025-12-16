dd7c003d17701460063fe1ce88313d3dcce2031d
Monu Kumar <57959076+MonuKumar1@users.noreply.github.com>
2025-12-16 17:55:50 +0530

    Time: 0 ms (100%), Space: 10.7 MB (48.63%) - LeetHub

---
+++ 0046-permutations/0046-permutations.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+
+     void helper(int idx, vector<int>& nums) {
+        if (idx == nums.size()) {
+            ans.push_back(nums);
+            return;
+        }
+
+        for (int i = idx; i < nums.size(); i++) {
+            swap(nums[idx], nums[i]);
+            helper(idx + 1, nums);
+            swap(nums[idx], nums[i]); 
+        }
+    }
+
+
+    vector<vector<int>> permute(vector<int>& nums) {
+        helper(0, nums);
+        return ans;
+    }
+};
