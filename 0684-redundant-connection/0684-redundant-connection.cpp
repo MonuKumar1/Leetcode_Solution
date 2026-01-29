4c8ab604f7c0ac97ec039e88a57438bdb5c96089
Monu Kumar <57959076+MonuKumar1@users.noreply.github.com>
2026-01-29 07:35:59 +0530

    Time: 3 ms (35.43%), Space: 12.8 MB (94.14%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.cpp
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -1,44 +1,31 @@
 class Solution {
 public:
-    vector<int>parent,rank;
-    
-
-    int findPar(int u){
-        if(u == parent[u])return u;
-
-        return parent[u] = findPar(parent[u]);
+    vector<int>parent;
+    int findParent(int node){
+        if(parent[node]==node)return node;
+        return parent[node] = findParent(parent[node]);
     }
-  
-        
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
-        
-        int n=edges.size();
+        int n = edges.size();
         parent.resize(n+1);
-        rank.resize(n+1,0);
-        vector<int>v(2);
-        
-        for(int i=1;i<=n;i++){
-            parent[i]=i;
+        for(int i=0;i<=n;i++){
+            parent[i] = i;
 
         }
-        
-        for(int i=0;i<n;i++)
-        {
-            int a=edges[i][0];
-            int b=edges[i][1];
-            int x = findPar(a), y= findPar(b);
-            if(x==y)
-            {
-                v[0]=a;
-                v[1]=b;
-                return v;
+
+        for(int i =0;i<n;i++){
+            int u = edges[i][0];
+            int v = edges[i][1];
+
+            int x = findParent(u), y = findParent(v);
+            
+            if(x==y){
+                return {u,v};
             }
             parent[x]=y;
-            
+
         }
-        
-        return v;
-        
-        
+
+return {0,0};
     }
 };
