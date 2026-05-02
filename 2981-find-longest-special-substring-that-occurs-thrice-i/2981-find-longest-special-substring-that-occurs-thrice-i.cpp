d3a9ebaec1bec90c4e4e70218c71f46fde57200f
Monu Kumar <57959076+MonuKumar1@users.noreply.github.com>
2026-05-02 08:47:29 +0530

    Time: 7 ms (69.35%), Space: 10.7 MB (76.34%) - LeetHub

---
+++ 2981-find-longest-special-substring-that-occurs-thrice-i/2981-find-longest-special-substring-that-occurs-thrice-i.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int maximumLength(string s) {
+        int n = s.length();
+        map<char, vector<int>> mp;
+
+        for (int i = 0; i < n; i++) {
+            char ch = s[i];
+            int cnt = 1;
+
+            while (i + 1 < n && ch == s[i + 1]) {
+                i++;
+                cnt++;
+            }
+            mp[ch].push_back(cnt);
+        }
+
+        int ans = -1;
+
+        for (auto &x : mp) {
+            vector<int> v = x.second;
+            int sz = v.size();
+            sort(v.begin(), v.end());
+            
+            ans = max(ans, v[sz-1] - 2);
+
+            if (sz >= 2) {
+                ans = max(ans, min(v[sz-1] - 1, v[sz-2]));
+            }
+
+            if (sz >= 3) {
+                ans = max(ans, min({v[sz-1], v[sz-2], v[sz-3]}));
+            }
+        }
+
+        return ans <= 0 ? -1 : ans;
+    }
+};
