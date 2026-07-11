65f152b0886067055a731843a42dcdf484778c9a
Monu Kumar <57959076+MonuKumar1@users.noreply.github.com>
2026-07-11 12:01:50 +0530

    Time: 289 ms (16.2%), Space: 12.7 MB (63.03%) - LeetHub

---
+++ 0473-matchsticks-to-square/0473-matchsticks-to-square.cpp
@@ -0,0 +1,33 @@
+class Solution {
+    vector<bool> used;
+    int target;
+    public:
+    bool backtrack(vector<int>& nums, int k, int currentSum, int start) {
+        if (k == 0)
+            return true;
+        if (currentSum == target)
+            return backtrack(nums, k - 1, 0, 0);
+
+        for (int i = start; i < nums.size(); i++) {
+            if (used[i] || currentSum + nums[i] > target)
+                continue;
+            used[i] = true;
+            if (backtrack(nums, k, currentSum + nums[i], i + 1))
+                return true;
+            used[i] = false;
+        }
+        return false;
+    }
+
+    bool makesquare(vector<int>& nums) {
+        int k =4;
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        if (sum % k != 0)
+            return false;
+
+        target = sum / k;
+        sort(nums.rbegin(), nums.rend());
+        used.assign(nums.size(), false);
+        return backtrack(nums, k, 0, 0);
+    }
+};
